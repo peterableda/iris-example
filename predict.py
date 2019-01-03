bd06006dc5981d227850a1c7fd6a377b7111a295
Peter Ableda <peter.ableda@cloudera.com>
2019-01-03 10:00:11 +0100

    Fix code for updated sklearn

--- predict.py
+++ predict.py
@@ -8,7 +8,8 @@ import time
 model = pickle.load(open('model.pickle', 'rb'))
 
 def run(args):
-  iris_x = float(args.get('length'))
-  time.sleep(5)
+  iris_x = np.reshape(float(args.get('petal_length')), (-1,1))
   result = model.predict(iris_x)
+
+  time.sleep(5)
   return result[0][0]
